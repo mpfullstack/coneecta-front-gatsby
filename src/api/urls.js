8b9482198a15c08a2d09a7a27f299c1044460c6f
mpfullstack <info@marcperez.cat>
2020-05-01 17:16:52 +0200

    urls conf

---
+++ src/api/urls.js
@@ -0,0 +1,7 @@
+const base = 'http://localhost:3000';
+const professionalsUrl = `${base}/professionals`;
+const professionalProfileUrl = `${professionalsUrl}/:id`;
+
+export {
+  professionalProfileUrl
+};
