d307baaefb2fe032e3bf4bf84535b2810aafb7aa
mpfullstack <info@marcperez.cat>
2020-05-04 21:57:41 +0200

    Dispatch show api error action

--- src/features/professionalProfile/professionalProfileSagas.js
+++ src/features/professionalProfile/professionalProfileSagas.js
@@ -1,12 +1,19 @@
-import { all, takeLatest, put, call, fork } from 'redux-saga/effects';
+import { all, takeLatest, put, call, fork, delay } from 'redux-saga/effects';
 import { loadProfile, initProfile } from './professionalProfileSlice';
+import { showApiError, hideApiError } from '../global/globalSlice';
 import api from '../../api';
 
 function* onLoadProfile() {
   // We receive the professional id in payload
   yield takeLatest(loadProfile, function* ({ payload }) {
-    const profile = yield call(api.getProfessionalProfile, payload);
-    yield put(initProfile(profile));
+    const result = yield call(api.getProfessionalProfile, payload);
+    if (result.error) {
+      yield put(showApiError(result.error));
+      yield delay(5000);
+      yield put(hideApiError());
+    } else {
+      yield put(initProfile(result));
+    }
   });
 }
 
