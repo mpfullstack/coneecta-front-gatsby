5b86c52fd49a47097c94780cd83d9745074b09a5
mpfullstack <info@marcperez.cat>
2020-05-04 20:39:38 +0200

    Combine global reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import globalSliceReducer from '../features/global/globalSlice';
 import professionalProfileReducer from '../features/professionalProfile/professionalProfileSlice';
 
 export default combineReducers({
+  global: globalSliceReducer,
   professionalProfile: professionalProfileReducer
 });
