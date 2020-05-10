a48c3cb4d801a304dab64b6628a89c7e880303bc
mpfullstack <info@marcperez.cat>
2020-05-10 10:18:57 +0200

    Temporary redirect index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useEffect } from 'react'
+import { navigate, Link } from 'gatsby'
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Provider store={store}>
-    <Layout>
-      <SEO title='Home' />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to='/professional/?id=1'>Go to professional profile</Link>
-    </Layout>
-  </Provider>
-)
+const IndexPage = () => {
+  useEffect(() => {
+    navigate('/professional/?id=1');
+  });
+
+  return null;
+  /*
+  return (
+    <Provider store={store}>
+      <Layout>
+        <SEO title='Home' />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to='/professional/?id=1'>Go to professional profile</Link>
+      </Layout>
+    </Provider>
+  );*/
+}
 
 export default IndexPage
