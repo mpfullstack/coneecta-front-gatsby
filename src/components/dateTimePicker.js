414b1917d621afa2158fbacb1863f0daa5ae47e0
mpfullstack <info@marcperez.cat>
2020-05-10 10:16:38 +0200

    Date time picker components

--- src/components/dateTimePicker.js
+++ src/components/dateTimePicker.js
@@ -1,23 +1,73 @@
-import React, { Component } from 'react'
-import DatePicker from './datePicker/DatePicker';
+import React from 'react'
+import theme from '../theme';
+import DatePicker from './datePicker/datePicker';
+import TimePicker from './timePicker/timePicker';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const DatePickerWrapper = styled.div`
+  .monthYearLabel {
+    color: ${theme.dateTimePickerColor};
+  }
+
+  .dateDayItem {
+    &.selected {
+      color: ${theme.dateTimePickerColor};
+      border: 2px solid ${theme.dateTimePickerColor};
+    }
+  }
+
   .svg-inline--fa {
-    color: #374e8c !important;
+    color: ${theme.dateTimePickerColor};
     font-size: 32px;
   }
 `;
 
-const DateTimePicker = ({ booking }) => {
+const TimePickerWrapper = styled.div`
+  .picker-container {
+    width: 94%;
+    margin: 0 auto;
+
+    .picker-column {
+        .picker-item.picker-item-selected {
+        color: rgb(55, 78, 140);
+        font-weight: bold;
+      }
+    }
+
+    .picker-highlight:after, .picker-highlight:before {
+      background-color: rgb(55, 78, 140);
+    }
+  }
+`;
+
+const availableTimes = [
+  { value: '12:00', label: '12:00' },
+  { value: '12:30', label: '12:30' },
+  { value: '13:00', label: '13:00' },
+  { value: '13:30', label: '13:30' },
+  { value: '14:00', label: '14:00' },
+  { value: '14:30', label: '14:30' },
+  { value: '15:00', label: '15:00' }
+];
+
+const DateTimePicker = ({ booking, onSelectDate, onSelectTime }) => {
+
   return (
-    <Wrapper>
-      <DatePicker
-        getSelectedDay={value => console.log('select day', value)}
-        endDate={350}
-        labelFormat={"MMMM yyyy"}
-        color={"#374e8c"}/>
-    </Wrapper>
+    <>
+      <DatePickerWrapper>
+        <DatePicker
+          getSelectedDay={value => onSelectDate(String(value))}
+          endDate={90}
+          labelFormat={"MMMM yyyy"} />
+      </DatePickerWrapper>
+
+      <TimePickerWrapper>
+        <TimePicker
+          valueGroups={{ time: '13:00' }}
+          optionGroups={{ time: availableTimes }}
+          onSelectTime={onSelectTime} />
+      </TimePickerWrapper>
+    </>
   )
 }
 
