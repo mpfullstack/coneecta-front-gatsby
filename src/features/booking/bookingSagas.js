deafa4d92333f7359126343d98f621610a70a464
mpfullstack <info@marcperez.cat>
2020-05-10 12:42:46 +0200

    Booking actions and sagas for date time picker

--- src/features/booking/bookingSagas.js
+++ src/features/booking/bookingSagas.js
@@ -1,5 +1,5 @@
 import { all, takeLatest, put, call, fork, delay } from 'redux-saga/effects';
-import { selectService } from './bookingSlice';
+import { selectService, initAvailableDates, selectDate, initAvailableTimes } from './bookingSlice';
 import { showApiError, hideApiError } from '../global/globalSlice';
 import api from '../../api';
 
@@ -13,13 +13,28 @@ function* onSelectService() {
       yield delay(5000);
       yield put(hideApiError());
     } else {
-      yield put(initAvialableDates(result));
+      yield put(initAvailableDates(result));
+    }
+  });
+}
+
+function* onSelectDate() {
+  // We have selected date in payload
+  yield takeLatest(selectDate, function* ({ payload }) {
+    const result = yield call(api.getAvailableTimes, payload);
+    if (result.error) {
+      yield put(showApiError(result.error));
+      yield delay(5000);
+      yield put(hideApiError());
+    } else {
+      yield put(initAvailableTimes(result));
     }
   });
 }
 
 export default function* () {
   yield all([
-    fork(onSelectService)
+    fork(onSelectService),
+    fork(onSelectDate)
   ])
 };
--- src/features/booking/bookingSlice.js
+++ src/features/booking/bookingSlice.js
@@ -9,9 +9,9 @@ const bookingSlice = createSlice({
     'availableDates': [], // Dates available for the selected service
     'availableTimes': [], // Times available for the selected service and date
     'date': '', // Booking date selected
-    'fetchingAvailableDates': false,
+    'fetchingAvailableDates': true,
     'time': '', // Booking time selected
-    'fetchingAvailableTimes': false
+    'fetchingAvailableTimes': true
 	},
   reducers: {
     // loadBooking: state => state,
@@ -19,9 +19,20 @@ const bookingSlice = createSlice({
     selectService: (state, action) => {
       state.serviceId = action.payload.serviceId;
       state.modalityType = action.payload.modalityType;
+      state.fetchingAvailableDates = true;
+    },
+    initAvailableDates: (state, action) => {
+      state.availableDates = action.payload;
+      state.fetchingAvailableDates = false;
     },
     selectDate: (state, action) => {
       state.date = action.payload;
+      state.fetchingAvailableTimes = true;
+      state.time = '';
+    },
+    initAvailableTimes: (state, action) => {
+      state.availableTimes = action.payload;
+      state.fetchingAvailableTimes = false;
     },
     selectTime: (state, action) => {
       state.time = action.payload;
@@ -32,7 +43,9 @@ const bookingSlice = createSlice({
 export const {
   selectService,
   selectDate,
-  selectTime
+  selectTime,
+  initAvailableDates,
+  initAvailableTimes
 } = bookingSlice.actions
 
 export default bookingSlice.reducer;
