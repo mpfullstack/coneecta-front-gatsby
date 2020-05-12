75a160ede810b2fb95acc2ccc7f623352172e43a
mpfullstack <info@marcperez.cat>
2020-05-12 18:16:06 +0200

    Added serverless function professionals

---
+++ api/professionals.js
@@ -0,0 +1,101 @@
+module.exports = (req, res) => {
+  res.json({
+    "professionals": [{
+      "id": 1,
+      "details": {
+        "name": "Isabela Reinket",
+        "avatar": "/img/profile1.jpg",
+        "rating": 4,
+        "country": "de",
+        "languages": [
+            "es",
+            "en",
+            "ca"
+        ]
+      },
+      "services": [
+        {
+          "id": 1,
+          "name": "Lectura del tarot",
+          "modalities": [
+            {
+              "id": 1,
+              "type": "videoconference",
+              "duration": 30,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            },
+            {
+              "id": 2,
+              "type": "audioconference",
+              "duration": 45,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            },
+            {
+              "id": 3,
+              "type": "onsite",
+              "duration": 30,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Sesion personalizada",
+          "modalities": [
+            {
+              "id": 1,
+              "type": "videoconference",
+              "duration": 30,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            },
+            {
+              "id": 2,
+              "type": "audioconference",
+              "duration": 45,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            },
+            {
+              "id": 3,
+              "type": "onsite",
+              "duration": 30,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "name": "Analisis astrologico",
+          "modalities": [
+            {
+              "id": 1,
+              "type": "videoconference",
+              "duration": 30,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            },
+            {
+              "id": 2,
+              "type": "audioconference",
+              "duration": 45,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            },
+            {
+              "id": 3,
+              "type": "onsite",
+              "duration": 30,
+              "credits": 123,
+              "credits_in_euros": "12,30"
+            }
+          ]
+        }
+      ]
+    }]
+  })
+}
