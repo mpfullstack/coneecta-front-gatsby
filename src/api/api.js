0470b7d29f8e37b7c41909ab9565afbd46dfd1cc
mpfullstack <info@marcperez.cat>
2020-05-18 16:52:52 +0200

    Removed unused code

--- src/api/api.js
+++ src/api/api.js
@@ -4,14 +4,7 @@ import {
 } from './urls';
 
 async function getProfessionalProfile(id) {
-  return await SuperFetch.get(professionalProfileUrl.replace(':id', id))
-    .then(response => {
-      if( response.status >= 400 ) {
-        // Handle type of error and translate message to spanish
-        // response.message = t(response.message, {}, 'Unexpected error');
-      }
-      return response;
-    });
+  return await SuperFetch.get(professionalProfileUrl.replace(':id', id));
 };
 
 async function getAvailableDates({ serviceId, modalityType}) {
