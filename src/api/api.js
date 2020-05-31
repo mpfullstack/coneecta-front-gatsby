9c4010088f80bd3d05776c074e6ee4292dcad91d
mpfullstack <info@marcperez.cat>
2020-05-31 17:49:19 +0200

    New api calls

--- src/api/api.js
+++ src/api/api.js
@@ -1,27 +1,23 @@
 import SuperFetch from '../helpers/superFetch';
 import {
-  professionalProfileUrl
+  professionalProfileUrl, timeZonesUrl, availableDatesUrl
 } from './urls';
 
 async function getProfessionalProfile(id) {
   return await SuperFetch.get(professionalProfileUrl.replace(':id', id));
 };
 
-async function getAvailableDates({ serviceId, modalityType}) {
-  // TODO: Implment call to API
-  // return await SuperFetch.get(professionalProfileUrl.replace(':id', id))
-  //   .then(response => {
-  //     if( response.status >= 400 ) {
-  //       // Handle type of error and translate message to spanish
-  //       // response.message = t(response.message, {}, 'Unexpected error');
-  //     }
-  //     return response;
-  //   });
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve([]), 300);
-  });
+async function getAvailableTimezones() {
+  return await SuperFetch.get(timeZonesUrl);
 }
 
+async function getAvailableDates({ timezone, serviceId }) {
+  return await SuperFetch.get(availableDatesUrl
+    .replace(':timezone', timezone)
+    .replace(':serviceId', serviceId));
+}
+
+// TODO: No necessary anymore
 async function getAvailableTimes({ date }) {
   // TODO: Implment call to API
   // return await SuperFetch.get(professionalProfileUrl.replace(':id', id))
@@ -39,6 +35,7 @@ async function getAvailableTimes({ date }) {
 
 const api = {
   getProfessionalProfile,
+  getAvailableTimezones,
   getAvailableDates,
   getAvailableTimes
 };
--- src/api/urls.js
+++ src/api/urls.js
@@ -1,7 +1,11 @@
 const base = process.env.GATSBY_API_URL;
 const professionalsUrl = `${base}/profile`;
 const professionalProfileUrl = `${professionalsUrl}?slug=:id`;
+const availableDatesUrl = `${base}/availability?timezone=:timezone&service=:serviceId`
+const timeZonesUrl = `${base}/timezones`;
 
 export {
-  professionalProfileUrl
+  professionalProfileUrl,
+  availableDatesUrl,
+  timeZonesUrl
 };
