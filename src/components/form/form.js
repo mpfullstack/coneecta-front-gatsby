f9f6c76ab22b5bca8b5d3cfe855f5912cc4aa8e8
mpfullstack <info@marcperez.cat>
2020-06-14 13:26:06 +0200

    Base form component

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useFormState } from 'react-use-form-state';
+import theme from '../../theme';
 
-export default ({ onSubmit, formData, renderForm, isFormValid }) => {
+const FormWrapper = styled.div`
+  .form-control:focus {
+      border-color: ${theme.inputBorderColorFocus};
+      box-shadow: 0 0 0 0.2rem ${theme.inputBoxShadowColorFocus};
+  }
+  .form-control.is-valid:focus {
+    border-color: #28a745;
+    box-shadow: 0 0 0 0.2rem rgba(40,167,69,.25);
+  }
+  .form-control.is-invalid:focus {
+    border-color: #dc3545;
+    box-shadow: 0 0 0 0.2rem rgba(220,53,69,.25);
+  }
+  .form-group {
+    margin-bottom: 0;
+    min-height: 70px;
+  }
+`;
+
+const Form = ({ onSubmit, formData, renderForm }) => {
   const [formState, input] = useFormState(formData);
 
   function handleSubmit(e) {
-    onSubmit(formState.values);
-    e.preventDefault();
+    if (typeof onSubmit === 'function') {
+      onSubmit(e, formState.values);
+      e.preventDefault();
+      e.stopPropagation();
+    }
   }
 
   // function disableForm() {
@@ -24,8 +48,12 @@ export default ({ onSubmit, formData, renderForm, isFormValid }) => {
   // }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {renderForm(formState, input)}
-    </form>
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        {renderForm(formState, input)}
+      </form>
+    </FormWrapper>
   );
 };
+
+export default Form;
