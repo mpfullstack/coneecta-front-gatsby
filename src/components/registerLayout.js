9159b48bd84587825a693101d63fcbbe63d84ce1
mpfullstack <info@marcperez.cat>
2020-06-14 13:25:57 +0200

    Register layout

--- src/components/registerLayout.js
+++ src/components/registerLayout.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import { Link } from 'gatsby';
+import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { loadProfile } from '../features/professionalProfile/professionalProfileSlice';
 import ProfileHeader from './professionalProfile/profileHeader';
@@ -16,9 +18,34 @@ const mapStateToProps = state => {
   }
 }
 
+const RegisterWrapper = styled.div`
+  .register-nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 0 12px 0;
+    a {
+      font-size: 20px;
+      display: inline-block;
+      &:first-child:after {
+        content: '|';
+        position: relative;
+        left: 10px;
+      }
+      &:last-child {
+        margin-left: 20px;
+      }
+      &.selected {
+        font-weight: bold;
+      }
+    }
+  }
+`;
+
 export const RegisterLayout = ({ profile, loadProfile, location, children }) => {
   // TODO: Check if it work on build production as location is not ready
   const slug = Query.getParams(location).slug;
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (slug !== '') {
@@ -30,22 +57,32 @@ export const RegisterLayout = ({ profile, loadProfile, location, children }) => 
 
   const profileDetails = profile.details || {};
 
+  function getClassName(section) {
+    if (location.pathname.indexOf(section) !== -1) {
+      return { className: 'selected' };
+    } else {
+      return {};
+    }
+  }
+
   return (
-    <Container>
-      {profile.id ?
-        <ProfileHeader id={profile.id} slug={slug} {...profileDetails} collapse={true} />
-        : null}
-      <Row className='justify-content-md-center'>
-        <Col xs='12' md='10'>
-          <Booking slug={slug} />
-          <nav>
-            <Link to={`/login?slug=${slug}`}>Login</Link>
-            <Link to={`/signup?slug=${slug}`}>Sign Up</Link>
-          </nav>
-          {children}
-        </Col>
-      </Row>
-    </Container>
+    <RegisterWrapper>
+      <Container>
+        {profile.id ?
+          <ProfileHeader id={profile.id} slug={slug} {...profileDetails} collapse={true} />
+          : null}
+        <Row className='justify-content-md-center'>
+          <Col xs='12' md='10'>
+            <Booking slug={slug} />
+            <nav className='register-nav'>
+              <Link {...getClassName('login')} to={`/login?slug=${slug}`}>{t('login')}</Link>
+              <Link {...getClassName('signup')} to={`/signup?slug=${slug}`}>{t('signup')}</Link>
+            </nav>
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    </RegisterWrapper>
   );
 }
 
