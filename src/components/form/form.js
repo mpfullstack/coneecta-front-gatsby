12b3d8ddfd7444bc9ac0a788eded1a3a533dcfb1
mpfullstack <info@marcperez.cat>
2020-08-30 09:43:30 +0200

    Moved styles to formControl

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,21 +1,8 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useFormState } from 'react-use-form-state';
-import theme from '../../theme';
 
 const FormWrapper = styled.div`
-  .form-control:focus {
-      border-color: ${theme.inputBorderColorFocus};
-      box-shadow: 0 0 0 0.2rem ${theme.inputBoxShadowColorFocus};
-  }
-  .form-control.is-valid:focus {
-    border-color: #28a745;
-    box-shadow: 0 0 0 0.2rem rgba(40,167,69,.25);
-  }
-  .form-control.is-invalid:focus {
-    border-color: #dc3545;
-    box-shadow: 0 0 0 0.2rem rgba(220,53,69,.25);
-  }
   .form-group {
     margin-bottom: 0;
     min-height: 70px;
--- src/components/form/formControl.js
+++ src/components/form/formControl.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Col, Form as RBForm } from 'react-bootstrap';
+import theme from '../../theme';
+
+const FormControlWrapper = styled.div`
+  .form-control:focus {
+      border-color: ${theme.inputBorderColorFocus};
+      box-shadow: 0 0 0 0.2rem ${theme.inputBoxShadowColorFocus};
+  }
+  .form-control.is-valid:focus {
+    border-color: #28a745;
+    box-shadow: 0 0 0 0.2rem rgba(40,167,69,.25);
+  }
+  .form-control.is-invalid:focus {
+    border-color: #dc3545;
+    box-shadow: 0 0 0 0.2rem rgba(220,53,69,.25);
+  }
+`;
 
 export default ({ label, placeholder, name, error, isValid, as = 'input', children = null, ...rest}) => {
   return (
-    <RBForm.Group as={Col} md='4' controlId={name}>
-      {label ? <RBForm.Label>{label}</RBForm.Label> : null}
-      <RBForm.Control placeholder={placeholder} {...isValid} as={as} {...rest}>{children}</RBForm.Control>
-      <RBForm.Control.Feedback type={isValid.isValid ? 'valid' : 'invalid'}>
-        {error}
-      </RBForm.Control.Feedback>
-    </RBForm.Group>
+    <FormControlWrapper>
+      <RBForm.Group as={Col} md='4' controlId={name}>
+        {label ? <RBForm.Label>{label}</RBForm.Label> : null}
+        <RBForm.Control placeholder={placeholder} {...isValid} as={as} {...rest}>{children}</RBForm.Control>
+        {isValid ? <RBForm.Control.Feedback type={isValid.isValid ? 'valid' : 'invalid'}>
+          {error}
+        </RBForm.Control.Feedback>
+        : null}
+      </RBForm.Group>
+    </FormControlWrapper>
   );
 }
