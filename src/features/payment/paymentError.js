41e68369f7775165b1e5341458b42794a611e777
mpfullstack <info@marcperez.cat>
2020-11-07 10:05:38 +0100

    New payment error page

---
+++ src/features/payment/paymentError.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container, Row, Col } from 'react-bootstrap';
+// import { useTranslation } from 'react-i18next';
+import SEO from '../../components/seo';
+import { ExclamationCircle } from '../../components/icons/icons';
+
+const PaymentErrorWrapper = styled.div`
+  .fa-exclamation-circle {
+    margin-top: 20px;
+    color: #e7b142;
+    font-size: 50px;
+  }
+  .confirm-text {
+    font-size: 20px;
+    margin: 20px 0;
+  }
+  .text {
+    margin-top: 40px;
+  }
+`;
+
+const PaymentError = ({ id }) => {
+  // const { t } = useTranslation();
+
+  return (
+    <PaymentErrorWrapper>
+      <Container>
+        <SEO title='Pago fallido' />
+        <Row className='justify-content-center text-center'>
+          <Col>
+            <ExclamationCircle />
+            <p className='confirm-text'><strong>¡Algo no ha ido bien!<br />({id})</strong></p>
+          </Col>
+        </Row>
+        <Row className='justify-content-center text-center'>
+          <Col xs='9'>
+            <p>Hemos identificado un problema con tu pago y no hemos podido confirmar tu reserva.</p>
+          </Col>
+        </Row>
+        <Row className='justify-content-center text-center text'>
+          <Col xs='2'></Col>
+          <Col xs='8'>Si quieres que te ayudemos a resolverlo, utiliza nuestro formulario de contacto para
+          contarnos tu caso y nuestro equipo se pondrá en contacto contigo lo antes posible.</Col>
+          <Col xs='2'></Col>
+        </Row>
+      </Container>
+    </PaymentErrorWrapper>
+  );
+}
+
+export default PaymentError;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -6,6 +6,7 @@ import store, { persistor } from '../redux/store';
 import Profile from '../features/profile';
 import ProfileBookings from '../features/profile/profileBookings';
 import PaymentConfirmed from '../features/payment/paymentConfirmed';
+import PaymentError from '../features/payment/paymentError';
 import Payment from '../features/payment';
 import PaymentCheckout from '../features/payment/paymentCheckout';
 import PrivateRoute from '../components/privateRoute';
@@ -23,8 +24,8 @@ export default () => {
             <PrivateRoute path="/bookings/:id/:action" component={ProfileBookings} />
             <PrivateRoute path="/payment" component={Payment} />
             <PrivateRoute path="/payment_checkout" component={PaymentCheckout} />
-            <PrivateRoute path="/payment_ok" component={PaymentConfirmed} />
             <PrivateRoute path="/payment_ok/:id" component={PaymentConfirmed} />
+            <PrivateRoute path="/payment_ko/:id" component={PaymentError} />
           </Router>
         </Layout>
       </PersistGate>
