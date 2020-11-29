1b8ad6d1363160f4626325e26e42348e31626ecb
mpfullstack <info@marcperez.cat>
2020-11-29 18:49:19 +0100

    Author avatar and name

--- src/features/profile/sessionActivity.js
+++ src/features/profile/sessionActivity.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import Skeleton from '../../components/skeleton';
+import ImageSkeleton from '../../components/imageSkeleton';
 import theme from '../../theme';
 
 const SessionActivityWrapper = styled.div`
@@ -26,31 +27,22 @@ const SessionActivityWrapper = styled.div`
   .date {
     font-style: italic;
   }
-  .movement-title {
+  .author {
     font-weight: 800;
     margin-bottom: 0px;
     text-align: left;
-  }
-  .credits {
-    font-weight: 800;
   }
 `;
 
 export default ({ activity }) => {
   if (activity) {
     return (
-      // <WalletMovementWrapper>
-      //   <div className='text movement-title'>{movement.title}</div>
-      //   <div className='text description'>{movement.description}</div>
-      //   <div className='text date'>
-      //     {format(new Date(movement.date), "d 'de' LLLL 'de' yyyy · H:mm 'horas'", { locale: es })}
-      //   </div>
-      //   <div className='credits'>{`${Math.abs(movement.credits)} créditos`}</div>
-      // </WalletMovementWrapper>
       <SessionActivityWrapper>
         <div className='text date'>
           {format(new Date(activity.date), "d 'de' LLLL 'de' yyyy · H:mm 'horas'", { locale: es })}
         </div>
+        <ImageSkeleton url={activity.author.avatar} circle={true} width={65} height={65} />
+        <div className='text author'>{activity.author.name}</div>
         <div className='text description'>{activity.body}</div>
       </SessionActivityWrapper>
     );
