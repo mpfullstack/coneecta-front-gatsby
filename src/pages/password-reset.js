5d17ad61d84b6e423730a41002883791ed5e39c3
mpfullstack <info@marcperez.cat>
2021-06-03 07:41:28 +0200

    Reset password page

---
+++ src/pages/password-reset.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { Location } from '@reach/router';
+import { Provider } from 'react-redux';
+import store, { persistor } from '../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+// import PasswordReset from '../features/loginSignUp/passwordResetForm';
+
+export default () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <SEO title='Password reset' />
+          {/* <Location>
+            {props => <PasswordReset {...props} />}
+          </Location> */}
+        </Layout>
+      </PersistGate>
+    </Provider>
+  );
+};
